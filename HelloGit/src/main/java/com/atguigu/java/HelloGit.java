0a46fccd1c2226c536e93e0070dd8917b319b94c
tom_glb <tom_global@163.com>
2020-06-07 11:13:41 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/java/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.java;
+
+/**
+ * @author WangCheng
+ * @describe
+ * @create 2020-06-07 11:03
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
